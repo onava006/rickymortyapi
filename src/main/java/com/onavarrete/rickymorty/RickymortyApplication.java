1dc4d3b4f0b89658d7fd73c61e71261ae53c00c9
rebrotescoicoi <rebrotescoicoi@gmail.com>
2024-06-20 16:48:10 -0400

    agregando primer commit proyecto inicializado spring initializr

---
+++ src/main/java/com/onavarrete/rickymorty/RickymortyApplication.java
@@ -0,0 +1,13 @@
+package com.onavarrete.rickymorty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RickymortyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RickymortyApplication.class, args);
+	}
+
+}
